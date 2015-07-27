f32c876f64f31fddd5729ee1dc0932ea95af1e60
luisadeva <luis.adeva@gmail.com>
2015-07-27 14:19:30 +0200

    Instalado Sequelizee, SQL local sqlite y postgres desa

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -1,16 +1,27 @@
+var models = require('../models/models.js');
+
 exports.question = function (req, res) {
-    console.log("LLEGA quiz-controller question");
-    res.render('quizes/question', {pregunta: "Capital de Italia"});
+    models.Quiz.findAll().then(
+        function (quiz) {
+            res.render('quizes/question', {pregunta: quiz[0].pregunta});
+        }
+    );
 };
 
 
 exports.answer = function (req, res) {
     console.log("LLEGA quiz-controller asnswer");
-    if (req.query.respuesta === "Roma") {
-        res.render("quizes/answer", {respuesta: "Correcto"});
-    } else {
-        res.render("quizes/answer", {respuesta: "Incorrecto"});
-    }
+    models.Quiz.findAll().then(
+        function(quiz) {
+            if (req.query.respuesta === quiz[0].respuesta) {
+                res.render("quizes/answer", {respuesta: "Correcto"});
+            } else {
+                res.render("quizes/answer", {respuesta: "Incorrecto"});
+            }
+        }
+    );
+    
+    
 }
 
 exports.creditos = function (req, res) {
